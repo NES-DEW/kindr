b974084c8bfd2f8f3e3785bac30d2a33833d52cd
Brendan Clarke <brendan.clarke2@nhs.scot>
2025-10-10 11:16:24 +0100

    added kind_links

---
+++ R/kind_links.R
@@ -0,0 +1,21 @@
+#' KIND links
+#'
+#' @returns an Rmarkdown/Quarto text slug containing KIND network info and links
+#' @export
+#'
+#' @examples kind_links()
+kind_links <- function(){
+
+
+
+  cat("The KIND network is an inclusive social learning community for people working with knowledge, information, and data across health, social care, and housing in Scotland.
+
+   \n
+
+  * [Join our Teams channel](https://forms.office.com/pages/responsepage.aspx?id=veDvEDCgykuAnLXmdF5Jmn79kl25VpJIq3eErXXCYKBUMUpENjBJOENINDRFMUlYQTlCM1RVNzRSRy4u&route=shorturl) for updates, technical support, and events (or see our [FAQ pages](https://nes-dew.github.io/KIND-community-standards/) for more info)
+  * [Subscribe to our mailing list](https://forms.office.com/pages/responsepage.aspx?id=veDvEDCgykuAnLXmdF5JmpopIZB9ynRJnrPUHVFccipURjM2NkZJUkhGOFlQRjQxRFhVUTgwT0UwVyQlQCN0PWcu) to receive a weekly update about our training and activities
+  * Visit our [training mini-site](https://nes-dew.github.io/KIND-training/) to find out about our free technical training, including Excel, Power BI, R, and SQL
+  * Find out more about the [digitally-enabled workforce programme](https://learn.nes.nhs.scot/53965/digitally-enabled-workforce), which operates the KIND network
+
+  ")
+}
